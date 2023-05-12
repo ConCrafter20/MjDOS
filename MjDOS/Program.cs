cf3fb2a9eb64b96d5f208fba2ef683edcdc73367
BuildTools <unconfigured@null.spigotmc.org>
2023-05-12 17:43:15 +0200

    Projektdateien hinzufügen.

---
+++ MjDOS/Program.cs
@@ -0,0 +1,126 @@
+﻿Console.WriteLine("               Welcome to the MjDOS installer!");
+Console.WriteLine(" ");
+Console.WriteLine("               !!PLEASE NOTE THAT THIS PROGRAM IS ONLY COSMETIC!!");
+Console.WriteLine(" ");
+Console.WriteLine("               Do you want to install MjDOS now? [y/n] ");
+string DYWTIMjDOS = Console.ReadLine();
+
+if (DYWTIMjDOS == "y" || DYWTIMjDOS == "Y")
+{
+    Console.Clear();
+
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    /");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    -");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine(@"    \");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    |");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    /");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    -");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine(@"    \");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    |");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    /");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    -");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine(@"    \");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    |");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    /");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    -");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine(@"    \");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    |");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Preparing Installation");
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine("    /");
+    Thread.Sleep(1000);
+
+    Console.Clear();
+
+    Console.
+    
+}
+else
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Quitting with exitcode 0");
+    Thread.Sleep(5000);
+    Environment.Exit(0);
+}
